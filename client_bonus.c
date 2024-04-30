25d2ac4ea15b4e0d5a0c0bddd1514a856dd2990e
Natália Medeiros Da Silva De Almeida <natalia@Natalias-MBP.home>
2024-04-30 14:20:14 +0200

    implemented answer signal

--- client_bonus.c
+++ client_bonus.c
@@ -1,21 +1,44 @@
 #include "libft/libft.h"
 #include <signal.h>
+#define TIMEOUT 5000
+
+int	g_checksignal = 1;
+
+void	error_exit(char *message)
+{
+	ft_putstr_fd(message, 2);
+	exit(EXIT_FAILURE);
+}
+
+void	sig_confirm(int sig)
+{
+	if (sig == SIGUSR1)
+		g_checksignal = 1;
+	// else if (sig == SIGUSR2)
+	// 	write(STDOUT_FILENO, "Msg received\n", 14);
+}
 
 void	bit_read(int pid_server, int nb)
 {
 	int	count;
 	int	n;
+	int	i;
 
 	count = 0;
 	while (count < 8)
 	{
+		g_checksignal = 0;
+		i = 0;
 		n = nb >> count & 1;
 		if (n == 1)
 			kill(pid_server, SIGUSR1);
 		else
 			kill(pid_server, SIGUSR2);
-		usleep(1000);
 		count++;
+		while (i++ < 100000 && g_checksignal == 0)
+			usleep(10);
+		if (i == 100000)
+			error_exit("Error: Server did not respond.\n");
 	}
 }
 
@@ -25,15 +48,20 @@ int	main(int argc, char **argv)
 	int	i;
 
 	if (argc != 3)
-		return (ft_putstr_nl("Missing imput"), EXIT_FAILURE);
+		error_exit("Missing imput\n");
 	pid_server = ft_atoi(argv[1]);
 	if (pid_server <= 0)
-		return (ft_putstr_nl("Wrong PID"), EXIT_FAILURE);
+		error_exit("Invalid PID\n");
+	signal(SIGUSR1, sig_confirm);
+	//signal(SIGUSR2, sig_confirm);
 	i = 0;
 	while (argv[2][i] != '\0')
 	{
 		bit_read(pid_server, argv[2][i]);
 		i++;
 	}
+	bit_read(pid_server, '\0');
+	if (g_checksignal == 1)
+		ft_printf("Signal received back from server.\n");
 	return (0);
 }
--- server_bonus.c
+++ server_bonus.c
@@ -6,12 +6,30 @@
 /*   By: natalia <natalia@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/29 14:53:07 by natalia       #+#    #+#                 */
-/*   Updated: 2024/04/29 15:29:43 by natalia       ########   odam.nl         */
+/*   Updated: 2024/04/30 14:09:10 by natalia       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ************************************************************************** */
+/* SA_SIGINFO - This flag indicates that the signal handler should receive    */
+/*				addtional information when a signal is received. When this    */
+/*				information is defined, it includes a pointer to a siginfo_t  */
+/*				structure. This structure contains detailed information about */
+/*				the signal, such as the process's PID                         */
+/* SA_NODEFER - This flag explicitly indicates that signals won't be bloqued. */
+/*				This allows the same signal to be received and processed      */
+/*				again while a signal handler is running.                      */
+/* ************************************************************************** */
+
+
 #include "libft/libft.h"
 #include <signal.h>
+
+void	error_exit(char *message)
+{
+	ft_putstr_fd(message, 2);
+	exit(EXIT_FAILURE);
+}
 
 void	signal_handler(int signum, siginfo_t *info, void *context)
 {
@@ -19,8 +37,6 @@ void	signal_handler(int signum, siginfo_t *info, void *context)
 	static int				i;
 
 	(void) context;
-	(void) info;
-	// printf("%d\n", info->si_pid);
 	buff |= (signum == SIGUSR1) << i;
 	i++;
 	if (i == 8)
@@ -29,20 +45,22 @@ void	signal_handler(int signum, siginfo_t *info, void *context)
 		i = 0;
 		buff = 0;
 	}
-	// if (signum == SIGUSR1)
-	// 	kill(info->si_pid, SIGUSR1);
-	// else if (signum == SIGUSR2)
-	// 	kill(info->si_pid, SIGUSR2);
+	if (kill(info->si_pid, SIGUSR1) == -1)
+		error_exit("Error: fail to send the signal to client\n");
 }
 
 int	main(void)
 {
 	struct sigaction	sa;
 
+	sa.sa_flags = SA_SIGINFO | SA_NODEFER;
 	sa.sa_sigaction = &signal_handler;
-	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
+	if (sigemptyset(&sa.sa_mask) == -1)
+		error_exit("Error: function sigemptyset failled\n");
 	ft_printf("PID: %d\n", getpid());
+	if (sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
+		error_exit("Error: fail on setting up signal handler");
 	while (1)
 		pause();
 	return (0);
