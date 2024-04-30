15ca6215f7b0e25dd3cc6961f7675cb925cb39b4
Natália Medeiros Da Silva De Almeida <natalia@Natalias-MBP.home>
2024-04-30 19:50:56 +0200

    fixed norminette

--- libft/ft_strcharcat.c
+++ libft/ft_strcharcat.c
@@ -6,12 +6,11 @@
 /*   By: natalia <natalia@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/30 19:40:14 by natalia       #+#    #+#                 */
-/*   Updated: 2024/04/30 19:44:42 by natalia       ########   odam.nl         */
+/*   Updated: 2024/04/30 19:50:21 by natalia       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 
 char	*ft_strcharcat(char *dest, const char src)
 {
